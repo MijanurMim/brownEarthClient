9f8cc6a59e707366e4b628739a540e347642dd23
MijanurMim <mijanurmim1@gmail.com>
2023-04-04 04:38:52 +0600

    back to top button implemented

--- src/App.js
+++ src/App.js
@@ -1,17 +1,20 @@
 import { BrowserRouter as Router } from "react-router-dom";
 import AnimatedRoutes from "./components/animatedRoutes/AnimatedRoutes";
 import Footer from "./components/footer/Footer";
+import GoToTop from "./components/goToTop/GoToTop";
 import Navbar from "./components/navbar/Navbar";
 import ScrollToTop from "./components/scrolToTop/ScrollToTop";
-
 function App() {
   return (
     <Router>
       {/* common  */}
+
       <ScrollToTop />
       <Navbar />
       <AnimatedRoutes />
       {/* Common */}
+
+      <GoToTop />
       <Footer />
     </Router>
   );
